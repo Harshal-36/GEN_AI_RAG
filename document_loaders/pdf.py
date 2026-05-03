e80061dd7dc8ab4b3fa3fd5afd3d5aaec48c2792
Harshal Nikumbh <harshal.nikumb36@gmail.com>
2026-05-04 00:08:14 +0530

    splitted a pdf and created chunks usint TokenTextSplitter

--- document_loaders/pdf.py
+++ document_loaders/pdf.py
@@ -1,9 +1,22 @@
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import TokenTextSplitter
 
 data = PyPDFLoader("document_loaders/HR_Round_Cheat_Sheet.pdf")
 docs = data.load()
 
-print(len(docs))
+splitter = TokenTextSplitter(
+  chunk_size = 50,
+  chunk_overlap = 5
+
+)
+
+chunks = splitter.split_documents(docs)
+
+print(len(chunks))
+
+# for i in chunks:
+#   print(i.page_content)
+#   print()
 
 
