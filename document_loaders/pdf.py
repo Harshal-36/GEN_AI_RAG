be07b106e7e5016ee45b2d625120e0faf4d350d0
Harshal Nikumbh <harshal.nikumb36@gmail.com>
2026-05-05 00:38:37 +0530

    implemented RecursiveCharacterTextSplitter

--- document_loaders/pdf.py
+++ document_loaders/pdf.py
@@ -1,19 +1,21 @@
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import TokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 data = PyPDFLoader("document_loaders/HR_Round_Cheat_Sheet.pdf")
 docs = data.load()
 
-splitter = TokenTextSplitter(
+splitter = RecursiveCharacterTextSplitter(
   chunk_size = 50,
-  chunk_overlap = 5
+  chunk_overlap = 10
 
 )
 
 chunks = splitter.split_documents(docs)
 
 print(len(chunks))
+print(chunks[0].page_content)
+print(chunks[1].page_content)
 
 # for i in chunks:
 #   print(i.page_content)
